f4404860a6137d5c8c00c3392b43cf51ca78cbb2
Aurora <gruberaurora@gmail.com>
2025-01-03 18:20:22 +0100

    Edit for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }
 
   return (
-  <BrowserRouter>
+  <BrowserRouter basename='/notalone'>
     <LanguageContext.Provider value={isItalian}>
       <Navigation />
       <Routes>
