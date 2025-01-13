47689996f0afe11955214c0aa65a2c28191eaada
FPKohitsuji <101195384+FPKohitsuji@users.noreply.github.com>
2025-01-13 14:26:48 +0100

    merge from Aurora Swipe

--- src/components/Al/AlHome.jsx
+++ src/components/Al/AlHome.jsx
@@ -2,20 +2,21 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import { Al } from './Al'
 
+
 function AlHome(props) {
-    const { chatStarted, startChat } = props;
-    return (
-      <>
-        {chatStarted ? (
-          <Al />
-        ) : (
-          <>
-            <div>Al Home</div>
-            <Link to='al' onClick={() => startChat(true)}>Chat</Link>
-          </>
-        )}
-      </>
-    );
-  }
+  const { chatStarted, startChat } = props;
+  return (
+    <>
+      {chatStarted ? (
+        <Al />
+      ) : (
+        <>
+          <div>Al Home</div>
+          <Link to='al' onClick={() => startChat(true)}>Chat</Link>
+        </>
+      )}
+    </>
+  );
+}
 
 export { AlHome }
