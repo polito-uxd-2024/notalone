8d7752572b765391fe7f867d6a1719a59843d352
FPKohitsuji <101195384+FPKohitsuji@users.noreply.github.com>
2025-01-15 19:36:57 +0100

    chat v2
    implementazione return to bottom ad ogni messaggio :p

--- src/components/Al/AlChat.jsx
+++ src/components/Al/AlChat.jsx
@@ -1,18 +1,19 @@
 //todo
-// 1. Creare i riquadri in css per utente e Al
+// 1. Creare i riquadri in css per utente e Al - FATTO
 // 2. riportare la schermata in basso quando diventa troppo lunga
 // 3. bloccare message input container in basso - FATTO
-// 4. sistemare la chat in modo che i messaggi siano divisi
-// 5. creare container per i messaggi + container per scrittura messaggio
+// 4. sistemare la chat in modo che i messaggi siano divisi - FATTO
+// 5. creare container per i messaggi + container per scrittura messaggio - la chat funziona anche senza container
 
 
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './Al.css';
 
 
 function AlChat() {
   const [message, setMessage] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
+  const chatBoxRef = useRef(null);
 
   const handleSendMessage = async () => {
     if (message.trim() === '') return;
@@ -38,6 +39,12 @@ function AlChat() {
 
   };
 
+  useEffect(() => {
+    if (chatBoxRef.current) {
+      chatBoxRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+    }, [chatHistory]);
+
   return (
     <div className="chat-container">
       <div className="chat-box">
@@ -46,8 +53,10 @@ function AlChat() {
             {msg.text}
           </div>
         ))}
+        
       </div>
-      <div className="input-box">
+      <div className="input-box"
+       ref={chatBoxRef}>
         <input
           type="text"
           value={message}
@@ -60,9 +69,12 @@ function AlChat() {
           }}
         />
         <button onClick={handleSendMessage}>Send</button>
+        <div ref={chatBoxRef}></div>
       </div>
     </div>
   );
 }
 
 export { AlChat };
+
+
