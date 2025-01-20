df8ff006f359e842d710eea0e441f58e443023da
Aurora <gruberaurora@gmail.com>
2025-01-20 17:15:04 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/notalone/sw.js', { scope: '/notalone/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
